c8fb9f01f11a6cb838386f35cb80978eea7c7912
popopocorn <popcorn375@naver.com>
2024-09-11 11:18:43 +0900

    1

--- graphics_lecture/warming4.cpp
+++ graphics_lecture/warming4.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
-
+#include<random>
+typedef struct board {
+	int what_card;
+	bool is_reverse;
+};
 class card_check {
-	int board[5][5]{};
-	
+private:
+	board chek_board[5][5]{};
+	int score{};
+
+public:
+	void print_board();
+	void select_board(int x, int y);
 };
