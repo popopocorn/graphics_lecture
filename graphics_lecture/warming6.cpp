30e294555bb5dfeec7a12ea754be8b10f04b6219
popopocorn <popcorn375@naver.com>
2024-09-22 20:27:22 +0900

    1

--- graphics_lecture/warming6.cpp
+++ graphics_lecture/warming6.cpp
@@ -3,6 +3,7 @@
 #include<random>
 #include<conio.h>
 #include<print>
+#include<stack>
 #define way_color SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
 #define obs_color SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
 #define player_color SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
@@ -12,11 +13,58 @@ char board[30][30]{};
 int player_x{};
 int player_y{};
 int dir[4][2] = {
-	(-1, 0), //╩С
-	(1, 0), //го
-	(0, -1), //аб
-	(0, 1) //©Л
+	{-1, 0}, //╩С
+	{1, 0 }, //го
+	{0, -1}, //аб
+	{0, 1 } //©Л
 };
+
+
+class player {
+private:
+	int player_x;
+	int player_y;
+
+public:
+	player() {
+		player_x = 0;
+		player_y = 0;
+	}
+
+	void go_w() {
+		if (player_y - 1 > -1 && board[player_y - 1][player_x] == '0') {
+			board[player_y][player_x] = '0';
+			--player_y;
+		}
+	}
+	void go_a() {
+		if (player_x - 1 > -1 && board[player_y][player_x - 1] == '0') {
+			board[player_y][player_x] = '0';
+			--player_x;
+		}
+	}
+	void go_s() {
+		if (player_y + 1 < 30 && board[player_y + 1][player_x] == '0') {
+			board[player_y][player_x] = '0';
+			++player_y;
+		}
+	}
+	void go_d() {
+		if (player_x + 1 < 30 && board[player_y][player_x + 1] == '0') {
+			board[player_y][player_x] = '0';
+			++player_x;
+		}
+	}
+	int get_x() {
+		return player_x;
+	}
+	int get_y() {
+		return player_y;
+	}
+};
+
+
+
 
 
 
@@ -33,9 +81,65 @@ void make_path();
 
 
 int main() {
-	init_board();
-	make_path();
-	print_board();
+	bool is_player = false;
+	player p1;
+	while (true) {
+		char menu;
+		if(_kbhit){
+			menu = _getch();
+			switch (menu) {
+			case 13:
+				init_board();
+				make_path();
+
+				break;
+
+			case 'r':
+
+				board[p1.get_y()][p1.get_x()] = '*';
+				is_player = true;
+
+				break;
+
+			case 'w':
+				if (is_player) {
+					p1.go_w();
+					board[p1.get_y()][p1.get_x()] = '*';
+				}
+
+				break;
+
+			case 'a':
+				if (is_player) {
+					p1.go_a();
+					board[p1.get_y()][p1.get_x()] = '*';
+				}
+	
+				break;
+
+
+			case 's':
+				if (is_player) {
+					p1.go_s();
+					board[p1.get_y()][p1.get_x()] = '*';
+				}
+	
+				break;
+
+
+			case 'd':
+				if (is_player) {
+					p1.go_d();
+					board[p1.get_y()][p1.get_x()] = '*';
+				}
+
+				break;
+
+			}
+		}
+		print_board();
+	}
+	
 	
 }
 
@@ -100,10 +204,83 @@ void init_board() {
 		}
 	}
 	board[0][0] = '0';
-	board[29][29] = '0';
 }
-
 
 void make_path() {
+	int cur_x = 0, cur_y = 0;
+	int step_now_dir = 0;
+	int last_dir = -1;
+	int direction, dx, dy;
+
+	std::stack<std::pair<int, int>> path_stack;
+	path_stack.push({ cur_x, cur_y });
+
+	srand((unsigned)time(0));
+	board[cur_y][cur_x] = 0;
+
+	while (cur_x < 29 || cur_y < 29) {
+		bool move = false;
+
+		for (int temp = 0; temp < 4; ++temp) {
+			direction = rand() % 4;
+			dy = dir[direction][0];
+			dx = dir[direction][1];
+
+			if (cur_y + dy >= 0 && cur_y + dy < 30 && cur_x + dx >= 0 && cur_x + dx < 30 &&
+				board[cur_y + dy][cur_x + dx] == '0') {
+				continue; 
+			}
+			if ((direction == 0 || direction == 2) && step_now_dir > 2) {
+				continue;
+			}
+			else if ((direction == 1 || direction == 3) && step_now_dir > 3) {
+				continue;
+			}
+			if (cur_y + dy >= 0 && cur_y + dy < 30 && cur_x + dx >= 0 && cur_x + dx < 30 &&
+				board[cur_y + dy][cur_x + dx] != 'X') {
+				cur_x += dx;
+				cur_y += dy;
+				board[cur_y][cur_x] = '0';  
+				step_now_dir++;
+				path_stack.push({ cur_x, cur_y });  
+
+			
+				if ((direction == 0 || direction == 2) && step_now_dir >= 3) {
+					step_now_dir = 0;
+					last_dir = -1;  
+				}
+				else if ((direction == 1 || direction == 3) && step_now_dir >= 4) {
+					step_now_dir = 0;
+					last_dir = -1;  
+				}
+				else {
+					last_dir = direction; 
+				}
+
+				move = true;  
+				break;
+			}
+		}
+
 	
+		if (!move) {
+			if (!path_stack.empty()) {
+				std::pair<int, int> prev_position = path_stack.top();
+				path_stack.pop();
+				cur_x = prev_position.first;
+				cur_y = prev_position.second;
+			}
+			else {
+				break; 
+			}
+		}
+
+		print_board();
+	}
+
+	
+
+
 }
+
+
